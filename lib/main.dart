b956e5f6d2c2a545dfb5d82d75fa1f93f4e34678
binivazqua <binizavazquez@gmail.com>
2024-04-04 22:25:20 -0600

    first material conceptual

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  // Change the background color:
+  //runApp(Container(color:Colors.lightGreen));
+  
+  //Use text widgets:
+  runApp(MaterialApp(
+    home: Material(
+      child: Text('hello wrld!'),
+    ),
+  ));
+}
+
