e7d5ce355797f22d2e0ef191b00bdbbc3e96a6ba
binivazqua <binizavazquez@gmail.com>
2024-04-08 15:02:22 -0600

    login and homepage

---
+++ lib/blocks/homePage.dart
@@ -0,0 +1,17 @@
+
+import 'package:flutter/material.dart';
+
+class homePage extends StatelessWidget {
+  const homePage({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body: Center(
+        child: Text('Hello again, User!'),
+      )
+    );
+  }
+  
+}
--- lib/blocks/loginPage.dart
+++ lib/blocks/loginPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutterin/blocks/homePage.dart';
 import 'package:flutterin/components/logoTile.dart';
 import 'package:flutterin/components/textField.dart';
 
@@ -11,85 +12,96 @@ class loginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: implement build
     return Center(
-      child: Column( // in Scaffold 'child' changes to 'body'.
-          // ALIGN ITEMS -> CSS
-      
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: EdgeInsets.all(5.0),
-              child: Icon(
-              Icons.interests,
-              size: 100,
-              color: Colors.purple[100],
-              
-                        ),
-            ),
-          Text('Hello again!',
-            style: TextStyle(
-              color: Colors.black,
-            )
-          ),
-          Text('Login to Continue',
-            style: TextStyle(
-              color: Colors.black,
-            )
-          ),
-          textField(
-            hintDesiredText: 'Username',
-            controller: usernameController,
-            obscureText: false,
-            ),
-          textField(
-            hintDesiredText: 'Password',
-            controller: passwordController,
-            obscureText: true,
-
-            ),
-          TextButton(
-            onPressed: () {
-              print('Button clicked.');
-            }, 
-            child: Text('New to Bejoy? Register!',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 12,
-                fontStyle: FontStyle.italic,
-              ),
-            ),
-          ),
-          ElevatedButton(
-            onPressed: ()
-          {
-            print('Log In Pressed');
-          },
-           child: Text('Go!',
-              style: TextStyle(
-              color: Colors.black,
-           ),
-           ),
-           style: ButtonStyle(
-            backgroundColor: MaterialStateProperty.resolveWith((states) => Colors.yellow[500])
-           ),
-           ),
-           Text('or ontinue with',
-              style: TextStyle(
-                fontSize: 12,
-                height: 3,
-              ),
-           ),
-           Row(
+        child: Column( // in Scaffold 'child' changes to 'body'.
+            // ALIGN ITEMS -> CSS
+        
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              logoTile(imagePath:'lib/images/google.png'),
-              SizedBox(width: 20.0),
-              logoTile(imagePath: 'lib/images/apple.png')
-            ],
-           ),
+              Padding(
+                padding: EdgeInsets.all(5.0),
+                child: Icon(
+                Icons.interests,
+                size: 100,
+                color: Colors.purple[100],
+                
+                          ),
+              ),
+            Text('Hello again!',
+              style: TextStyle(
+                color: Colors.black,
+              )
+            ),
+            Text('Login to Continue',
+              style: TextStyle(
+                color: Colors.black,
+              )
+            ),
+            textField(
+              hintDesiredText: 'Username',
+              controller: usernameController,
+              obscureText: false,
+              ),
+            textField(
+              hintDesiredText: 'Password',
+              controller: passwordController,
+              obscureText: true,
+      
+              ),
+            TextButton(
+              onPressed: () {
+                print('Button clicked.');
+              }, 
+              child: Text('New to Bejoy? Register!',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 12,
+                  fontStyle: FontStyle.italic,
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: ()
+            {
+              Navigator.of(context).push(MaterialPageRoute(
+                builder: (context) {
+                  return homePage();
+                  
 
-
-        ],
-        ),
+                 }
+                ));
+                Navigator.of(context).pushNamed(
+                  '/home'
+                );
+              
+            },
+             child: Text('Go!',
+                style: TextStyle(
+                color: Colors.black,
+             ),
+             ),
+             style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.resolveWith((states) => Colors.yellow[500])
+             ),
+             ),
+             Text('or ontinue with',
+                style: TextStyle(
+                  fontSize: 12,
+                  height: 3,
+                ),
+             ),
+             Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                logoTile(imagePath:'lib/images/google.png'),
+                SizedBox(width: 20.0),
+                logoTile(imagePath: 'lib/images/apple.png')
+              ],
+             ),
+      
+      
+          ],
+          ),
+      
     );
   }
 
