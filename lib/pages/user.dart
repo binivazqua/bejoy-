4b44979411fc5b8db4b5b1b6f9ce99b4c35fea0e
binivazqua <binizavazquez@gmail.com>
2024-04-09 13:30:37 -0600

    user view done

--- lib/pages/user.dart
+++ lib/pages/user.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class ProfilePage extends StatelessWidget {
 
@@ -47,14 +48,75 @@ class ProfilePage extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                CircleAvatar(
-                  radius: 50,
-                )
-              ],
-              )
+                Stack(
+                  
+                  alignment: Alignment.bottomRight,
+                  children: [ 
+                    CircleAvatar(
+                    radius: 50,
+                    
+                    ),
+                    InkWell(
+                      onTap: (){},
+                      child: CircleAvatar(
+                        backgroundColor: Colors.orange,
+                        radius: 12,
+                        child: Icon(
+                          Icons.edit, size: 15,
+                          
+                          color: Colors.pink
+                          )
+                          
+                        ),
+                    )
+                
+                ]),
+
+                Padding(
+                  padding: const EdgeInsets.only(top: 20.0, bottom: 10.0),
+                  child: Text('Username Profile', style: TextStyle(fontWeight: FontWeight.w600),),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Column(
+                      children: [
+                        Text('username 🤠', style: TextStyle(
+                            fontWeight: FontWeight.w200
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      Text('My Awards', style: TextStyle(
+                        fontSize: 15,
+                      ))
+                      ],
+                      )
+                  ]
+                ),
+                
+              ]),
+          ),
+          const SizedBox(height: 8),
+          Container(
+            color: Colors.pink[100],
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: 
+            Column(children: [
+                GridView.count(crossAxisCount: 2,
+                shrinkWrap: true,
+                childAspectRatio: 1/1.5,
+                children:[
+                  Icon(Icons.headphones),
+                  Icon(Icons.headphones),
+                  
+
+                ])
+            ],)
           ),
           
-      ]
+      ],
     ),
 
 
