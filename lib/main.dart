9ea6345871599d0e5dd7b84d73d0099c328d3775
binivazqua <binizavazquez@gmail.com>
2024-04-26 08:42:15 -0600

    mood tracker tuning

--- lib/main.dart
+++ lib/main.dart
@@ -16,18 +16,15 @@ Future<void> main() async {
   Google complements made for flutter.
   - We always start with MaterialApp() widgtes.
   - MaterialApp() requires Material() widgets.
-  */ 
+  */
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(const MyApp());
-  
 }
 
-
 class MyApp extends StatelessWidget {
-  
   const MyApp({super.key});
 
   @override
@@ -38,17 +35,62 @@ class MyApp extends StatelessWidget {
       home: loginPage(),
       //home: meditationTemplate(),
       //home: moodTracker(),
-      
+
       routes: {
-        '/home' : (context) => navHomePage(),
-        '/painRelief' : (context) => meditationTemplate(constants: new meditationConstants(color: Colors.blueAccent, timer: 10, hints: ["uno", "dos", "tres"], waveColor: Colors.indigo, duration: 2000, title: "Anger Relief",)), //meditationTemplate(title: "Anger Relief", color: Colors.orangeAccent, millis: 1000,),
-        '/energyBoost' : (context) => meditationTemplate(constants: new meditationConstants(color: Colors.pink, timer: 5, hints: ["cuatro", "cinco", "seis"], waveColor: Colors.yellowAccent, duration: 3000, title: "Energy Boost", )),//meditationTemplate(title: "Energy Boost", color: Color.fromARGB(255, 240, 235, 105), millis: 3000, ),
-        '/focusWave' : (context) => meditationTemplate(constants: new meditationConstants(color: Colors.purpleAccent, timer: 15, hints: ["siete", "ocho", "nueve"], waveColor: Colors.deepPurple, duration: 4000, title: "Focus Wave", )),//meditationTemplate(title: "Focus Wave", color: Color.fromARGB(255, 105, 112, 240), millis: 1000, ),
-        '/sos' : (context) => meditationTemplate(constants: new meditationConstants(color: Colors.pink, timer: 20, hints: ["diez", "once", "doce"], waveColor: Colors.pinkAccent, duration: 4500, title: "SOS", )), //meditationTemplate(title: "SOS", color: Color.fromARGB(255, 240, 105, 105), millis: 500, ),
+        '/home': (context) => navHomePage(),
+        '/painRelief': (context) => meditationTemplate(
+                constants: new meditationConstants(
+              color: Colors.blueAccent,
+              timer: 10,
+              hints: [
+                "Think of three reasons of why you may be angry.",
+                "Tension all your limbs. And let go...",
+                "Exercise Done."
+              ],
+              waveColor: Colors.indigo,
+              duration: 2000,
+              title: "Anger Relief",
+            )), //meditationTemplate(title: "Anger Relief", color: Colors.orangeAccent, millis: 1000,),
+        '/energyBoost': (context) => meditationTemplate(
+                constants: new meditationConstants(
+              color: Colors.pink,
+              timer: 5,
+              hints: [
+                "Smile! You are amazing!",
+                "Think of three things that excite you about today.",
+                "Great! You are done."
+              ],
+              waveColor: Colors.orangeAccent,
+              duration: 3000,
+              title: "Energy Boost",
+            )), //meditationTemplate(title: "Energy Boost", color: Color.fromARGB(255, 240, 235, 105), millis: 3000, ),
+        '/focusWave': (context) => meditationTemplate(
+                constants: new meditationConstants(
+              color: Colors.purpleAccent,
+              timer: 15,
+              hints: [
+                "Now focus on one object in front of you.",
+                "Ennumerate three things which make it unique.",
+                "Exhale deeply, and you are done."
+              ],
+              waveColor: Colors.deepPurple,
+              duration: 4000,
+              title: "Focus Wave",
+            )), //meditationTemplate(title: "Focus Wave", color: Color.fromARGB(255, 105, 112, 240), millis: 1000, ),
+        '/sos': (context) => meditationTemplate(
+                constants: new meditationConstants(
+              color: Colors.pink,
+              timer: 20,
+              hints: [
+                "Focus entirely on the echo of your heartbeat.",
+                "Try to sync you inhales with its rythm.",
+                "Repeat."
+              ],
+              waveColor: Colors.pinkAccent,
+              duration: 4500,
+              title: "SOS",
+            )), //meditationTemplate(title: "SOS", color: Color.fromARGB(255, 240, 105, 105), millis: 500, ),
       },
-      
     );
   }
-
 }
-
--- lib/pages/subs/moodTracker.dart
+++ lib/pages/subs/moodTracker.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
+
 class moodTracker extends StatefulWidget {
   const moodTracker({super.key});
 
@@ -9,11 +10,9 @@ class moodTracker extends StatefulWidget {
 }
 
 class _moodTrackerState extends State<moodTracker> {
-
   final _textController1 = TextEditingController();
   final _textController2 = TextEditingController();
   final _textController3 = TextEditingController();
-
 
   // save users track:
   String userPost = ' ';
@@ -24,97 +23,128 @@ class _moodTrackerState extends State<moodTracker> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(backgroundColor:  Color.fromARGB(0, 105, 240, 175)),
+      appBar: AppBar(backgroundColor: Color.fromARGB(0, 105, 240, 175)),
       body: Padding(
         padding: const EdgeInsets.all(30),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
-
             Padding(
-              padding: const EdgeInsets.only(bottom: 30),
-              child: Container(alignment: Alignment.center, child: Text('Express Mood Tracker')),
+              padding: const EdgeInsets.only(bottom: 40),
+              child: Container(
+                  alignment: Alignment.center,
+                  child: Text('Express Mood Tracker',
+                      style: TextStyle(
+                          fontSize: 20, fontWeight: FontWeight.bold))),
             ),
 
-            Container(alignment:Alignment.bottomLeft, child:  Text('¿Cómo te sientes en este momento?')),
+            Container(
+                alignment: Alignment.bottomLeft,
+                child: Text('¿Cómo te sientes en este momento?')),
             Padding(
-              padding: const EdgeInsets.only(bottom: 50),
-              child: TextField(
-                controller: _textController1,
-                decoration: InputDecoration(border: UnderlineInputBorder(),
-                hintText: '...',
-                suffixIcon: IconButton(onPressed: (){
-                  _textController1.clear();
-                }, icon: Icon(Icons.clear_rounded))
+              padding: const EdgeInsets.only(bottom: 50, top: 10),
+              child: Container(
+                padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                    color: Colors.purple[50],
+                    borderRadius: BorderRadius.circular(10)),
+                child: TextField(
+                  controller: _textController1,
+                  decoration: InputDecoration(
+                      border: UnderlineInputBorder(),
+                      hintText: '...',
+                      suffixIcon: IconButton(
+                          onPressed: () {
+                            _textController1.clear();
+                          },
+                          icon: Icon(Icons.clear_rounded))),
                 ),
               ),
             ),
 
-            Container(alignment:Alignment.bottomLeft, child:  Text('¿Cómo te sientes en este momento?')),
+            Container(
+                alignment: Alignment.bottomLeft,
+                child: Text('Rápidamente... ¿Qué piensas?')),
             Padding(
-              padding: const EdgeInsets.only(bottom: 50),
-              child: TextField(
-                controller: _textController2,
-                decoration: InputDecoration(border: UnderlineInputBorder(),
-                hintText: '...',
-                suffixIcon: IconButton(onPressed: (){
-                  _textController2.clear();
-                }, icon: Icon(Icons.clear_rounded))
+              padding: const EdgeInsets.only(bottom: 50, top: 10),
+              child: Container(
+                padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                    color: Colors.pink[50],
+                    borderRadius: BorderRadius.circular(10)),
+                child: TextField(
+                  controller: _textController2,
+                  decoration: InputDecoration(
+                      border: UnderlineInputBorder(),
+                      hintText: '...',
+                      suffixIcon: IconButton(
+                          onPressed: () {
+                            _textController2.clear();
+                          },
+                          icon: Icon(Icons.clear_rounded))),
                 ),
               ),
             ),
 
-            Container(alignment:Alignment.bottomLeft, child:  Text('¿Cómo te sientes en este momento?')),
+            Container(
+                alignment: Alignment.bottomLeft,
+                child: Text('¿Cómo te gustaría sentirte ahora?')),
             Padding(
-              padding: const EdgeInsets.only(bottom: 30),
-              child: TextField(
-                controller: _textController3,
-                decoration: InputDecoration(border: UnderlineInputBorder(),
-                hintText: '...',
-                suffixIcon: IconButton(onPressed: (){
-                  _textController3.clear();
-                }, icon: Icon(Icons.clear_rounded))
+              padding: const EdgeInsets.only(bottom: 30, top: 10),
+              child: Container(
+                padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                    color: Colors.orange[50],
+                    borderRadius: BorderRadius.circular(10)),
+                child: TextField(
+                  controller: _textController3,
+                  decoration: InputDecoration(
+                      border: UnderlineInputBorder(),
+                      hintText: '...',
+                      suffixIcon: IconButton(
+                          onPressed: () {
+                            _textController3.clear();
+                          },
+                          icon: Icon(Icons.clear_rounded))),
                 ),
               ),
             ),
-
-            
-
-            
-
-            
 
             MaterialButton(
-              onPressed: (){
+              color: Colors.grey[50],
+              focusColor: const Color.fromARGB(255, 163, 62, 62),
+              onPressed: () {
                 _textControlVerif1();
-                
               },
               child: Text('Save my tracker!'),
             ),
 
             // display text:
-            Container(child: Container(
-              child: Center(
-                child: Text(userPost),
-                )
-            )),
-
+            Container(
+                child: Container(
+                    child: Center(
+              child: Text(userPost),
+            ))),
           ],
         ),
       ),
     );
   }
 
-  void _textControlVerif1 (){
-    if(_textController1.text == 'bien' || _textController1.text == 'feliz' || _textController1.text == 'alegre'){
-          setState(() {
-          userPost = "Estás de buen humor.";
-          });
-    } else if(_textController1.text == 'mal' || _textController1.text == 'triste'|| _textController1.text == 'enojado'){
-          setState(() {
-          userPost = "Necesitamos trabajar en tus sentimientos.";
-          });
+  void _textControlVerif1() {
+    if (_textController1.text == 'bien' ||
+        _textController1.text == 'feliz' ||
+        _textController1.text == 'alegre') {
+      setState(() {
+        userPost = "Estás de buen humor.";
+      });
+    } else if (_textController1.text == 'mal' ||
+        _textController1.text == 'triste' ||
+        _textController1.text == 'enojado') {
+      setState(() {
+        userPost = "Necesitamos trabajar en tus sentimientos.";
+      });
     }
   }
 }
