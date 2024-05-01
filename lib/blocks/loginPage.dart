7e268c9dd0c0064a19805ce7e27fc37570dda91c
binivazqua <binizavazquez@gmail.com>
2024-05-01 11:09:43 -0600

    added provisional bee logo

--- lib/blocks/loginPage.dart
+++ lib/blocks/loginPage.dart
@@ -15,108 +15,91 @@ class loginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: implement build
     return Center(
-        child: Scaffold(
-          backgroundColor: Colors.white,
-          body:  
-              Column( // in Scaffold 'child' changes to 'body'.
-                // ALIGN ITEMS -> CSS
-                        
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(5.0),
-                    child: Icon(
-                    Icons.interests,
-                    size: 100,
-                    color: Colors.purple[100],
-                    
-                              ),
-                  ),
-                const Text('Hello again!',
-                  style: TextStyle(
-                    color: Colors.black,
-                  )
-                ),
-                const Text('Login to Continue',
-                  style: TextStyle(
-                    color: Colors.black,
-                  )
-                ),
-                textField(
-                  hintDesiredText: 'Username',
-                  controller: usernameController,
-                  obscureText: false,
-                  ),
-                textField(
-                  hintDesiredText: 'Password',
-                  controller: passwordController,
-                  obscureText: true,
-                  
-                  ),
-                TextButton(
-                  onPressed: () {
-                    print('Button clicked.');
-                  }, 
-                  child: const Text('New to Bejoy? Register!',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 12,
-                      fontStyle: FontStyle.italic,
-                    ),
-                  ),
-                ),
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: Column(
+          // in Scaffold 'child' changes to 'body'.
+          // ALIGN ITEMS -> CSS
 
-                //myButton(),
-                
-                ElevatedButton(
-                  onPressed: ()
-                {
-                  Navigator.of(context).push(MaterialPageRoute(
-                    builder: (context) {
-                      return const navHomePage();
-
-                        
-                     }
-                    ));
-                    Navigator.of(context).pushNamed(
-                      '/home'
-                    );
-                  
-                },
-                 style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.resolveWith((states) => Colors.yellow[500])
-                 ),
-                 child: const Text('Go!',
-                    style: TextStyle(
-                    color: Colors.black,
-                 ),
-                 ),
-                 ),
-
-                 
-                 const Text('or ontinue with',
-                    style: TextStyle(
-                      fontSize: 12,
-                      height: 3,
-                    ),
-                 ),
-                 const Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    logoTile(imagePath:'lib/images/google.png'),
-                    SizedBox(width: 20.0),
-                    logoTile(imagePath: 'lib/images/apple.png')
-                  ],
-                 ),
-                  
-                  
-              ],
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: Image(
+                  image: AssetImage('lib/images/bee.png'),
+                  width: 100,
+                )),
+            const Text('Hello again!',
+                style: TextStyle(
+                  color: Colors.black,
+                )),
+            const Text('Login to Continue',
+                style: TextStyle(
+                  color: Colors.black,
+                )),
+            textField(
+              hintDesiredText: 'Username',
+              controller: usernameController,
+              obscureText: false,
+            ),
+            textField(
+              hintDesiredText: 'Password',
+              controller: passwordController,
+              obscureText: true,
+            ),
+            TextButton(
+              onPressed: () {
+                print('Button clicked.');
+              },
+              child: const Text(
+                'New to Bejoy? Register!',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 12,
+                  fontStyle: FontStyle.italic,
+                ),
               ),
-            
+            ),
+
+            //myButton(),
+
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context)
+                    .push(MaterialPageRoute(builder: (context) {
+                  return const navHomePage();
+                }));
+                Navigator.of(context).pushNamed('/home');
+              },
+              style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.resolveWith(
+                      (states) => Colors.yellow[500])),
+              child: const Text(
+                'Go!',
+                style: TextStyle(
+                  color: Colors.black,
+                ),
+              ),
+            ),
+
+            const Text(
+              'or ontinue with',
+              style: TextStyle(
+                fontSize: 12,
+                height: 3,
+              ),
+            ),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                logoTile(imagePath: 'lib/images/google.png'),
+                SizedBox(width: 20.0),
+                logoTile(imagePath: 'lib/images/apple.png')
+              ],
+            ),
+          ],
         ),
-      
+      ),
     );
   }
-
-  
 }
