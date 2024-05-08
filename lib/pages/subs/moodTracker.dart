7250a27a7ac60c544609f632da2a8e5937081848
binivazqua <binizavazquez@gmail.com>
2024-05-08 16:16:10 -0600

    mood tracker init done

--- lib/pages/subs/moodTracker.dart
+++ lib/pages/subs/moodTracker.dart
@@ -7,6 +7,45 @@ class moodTrackerPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.only(top: 200),
+          child: Column(
+            children: [
+              Text('How are you feeling?'),
+              SizedBox(height: 5),
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.face),
+                    iconSize: 50,
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.face),
+                    iconSize: 50,
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.face),
+                    iconSize: 50,
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.face),
+                    iconSize: 50,
+                  ),
+                ],
+              ),
+              SizedBox(height: 10),
+              TextButton(onPressed: () {}, child: Text('Go!')),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
